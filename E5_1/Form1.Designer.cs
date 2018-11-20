b2297c89fbbc01babd5b7794a2747a1fe33c2ba1
Sergey Sokolov <Sergey.82.sokolov@gmail.com>
2018-11-20 23:15:36 +0300

    Convert vba-code to C#

---
+++ E5_1/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace E5_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(188, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(274, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Скопировать файлы";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(285, 245);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Каталог, в котором находятся прайсы";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(807, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(118, 125);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(188, 261);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(437, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(188, 186);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(436, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(349, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Файл-справочник";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(502, 298);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 47);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Отмена";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(184, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(454, 48);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Приложение создает временный каталог и\nкопирует в него файлы, сконвертировав в  X" +
+    "LSX";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Подготовка файлов";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ E5_1/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using OfficeOpenXml;
+using System.IO;
+
+namespace E5_1
+{
+    public partial class Form1 : Form
+    {
+
+        private string startDir1 = AppDomain.CurrentDomain.BaseDirectory;
+        private string startDir = "C:\\Users\\Sergey\\Desktop\\3";
+
+        
+        private string nomenclatureFile = "СПРАВОЧНИК  НОМЕНКЛАТУРЫ.xls";
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.textBox1.Text = startDir1;
+            this.textBox2.Text = nomenclatureFile;
+        }
+
+        private void CreateTmpFile(string FromFileName, string ToFilename)
+        {
+            var app = new Excel.Application();
+            CreateTmpFile(FromFileName, ToFilename, app);
+            app.Quit();
+        }
+
+        private void CreateTmpFile(string FromFileName, string ToFilename, Excel.Application app)
+        {
+
+            if (File.Exists(FromFileName))
+            { 
+                var DisplayAlerts = app.DisplayAlerts;
+                app.DisplayAlerts = false;
+                var wb = app.Workbooks.Open(Filename: FromFileName, ReadOnly:true);
+                wb.SaveAs(Filename: ToFilename, FileFormat: Excel.XlFileFormat.xlOpenXMLWorkbook, ConflictResolution: Excel.XlSaveConflictResolution.xlLocalSessionChanges);
+                wb.Close(false);
+                app.DisplayAlerts = DisplayAlerts;
+            }
+        }
+
+
+
+        private string CreateTmpDir(string startDir)
+        {
+            if (Directory.Exists(startDir))
+            {
+                int i = 1;
+                while (Directory.Exists(startDir + i)) i++;
+                Directory.CreateDirectory(startDir + i);
+                return startDir + i + "\\";
+            }
+            else return "";
+        }
+
+        //
+        // Создание временных копий файлов прайсов
+        private void CopyTmpFiles(string tmpDir)
+        {
+            FileInfo fi = new FileInfo(startDir + "#Описание.xlsx");
+            ExcelPackage ep = new ExcelPackage(fi);
+            ExcelWorkbook wb = ep.Workbook;
+
+            string xlsxFileName;
+            var app = new Excel.Application();
+
+
+            string A = "A";
+            var FileList = wb.Worksheets["Лист1"].Cells[A + "3:" + A + "100"].ToArray();
+            int i = 0;
+
+            while (i < FileList.Count())
+            {
+
+                if (FileList[i].Value == null)
+                {
+                    i++; continue;
+                }
+                Console.WriteLine(i + " - " + FileList[i].Value.ToString().Trim() + ".xlsx");
+
+                xlsxFileName = FileList[i].Value.ToString().Trim() + ".xlsx";
+
+                var chkFiles = Directory.GetFiles(startDir);
+
+                foreach (string filename in chkFiles)
+                {
+
+                    if (filename.ToLower() == (startDir + xlsxFileName).ToLower() ||
+                        filename.ToLower() == (startDir + xlsxFileName.Replace("ё", "е")).ToLower())
+                    {
+                        File.Copy(filename, tmpDir + xlsxFileName);
+                    }
+
+                    else if (filename.ToLower() == (startDir + xlsxFileName.Substring(0, xlsxFileName.Length - 1)).ToLower() ||
+                            filename.ToLower() == (startDir + xlsxFileName.Substring(0, xlsxFileName.Length - 1).Replace("ё", "е")).ToLower())
+                    {
+                        CreateTmpFile(filename, tmpDir + xlsxFileName, app);
+                    }
+
+                }
+
+                i++;
+            }
+            Console.WriteLine(i + " - " + FileList.Count());
+            app.Quit();
+        }
+
+        //private void Button1_Click(object sender, EventArgs e)
+        //{
+        //    startDir = this.textBox1.Text;
+        //    if (startDir.Substring(startDir.Length - 1) != "\\")
+        //    {
+        //        startDir = startDir+"\\";
+        //    }
+        //    string tmpDir = CreateTmpDir(startDir);
+
+        //    if (tmpDir != "")
+        //    {
+
+                
+        //        //CopyTmpFiles(tmpDir);
+        //        CreateTmpFile(startDir + "СПРАВОЧНИК НОМЕНКЛАТУРЫ.xls", tmpDir + "СПРАВОЧНИК НОМЕНКЛАТУРЫ.xlsx");
+        //    }
+
+
+        //    this.Close();
+        //}
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            startDir = this.textBox1.Text;
+            nomenclatureFile= this.textBox2.Text;
+            if (startDir.Substring(startDir.Length - 1) != "\\")
+            {
+                startDir = startDir + "\\";
+            }
+            string tmpDir = CreateTmpDir(startDir);
+
+            if (tmpDir != "")
+            {
+
+
+                CopyTmpFiles(tmpDir);
+                CreateTmpFile(startDir + nomenclatureFile, tmpDir + nomenclatureFile + "x");
+                MessageBox.Show("Файлы лежат здесь:\n" + tmpDir);
+            }
+            else
+            {
+                MessageBox.Show("Файлы не скопированы" );
+            }
+            
+
+            this.Close();
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
